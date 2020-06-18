808eccf687d70ffd57074da5cda43349a1107663
Adway Wadekar <adway@adway.io>
2020-06-17 20:19:22 -0400

    Finish milk2

---
+++ milk2/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Adway Wadekar on 6/17/20.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+#define MAXMILKING 5000
+
+int main()
+{
+}
--- milk2/milk2.py
+++ milk2/milk2.py
@@ -11,27 +11,53 @@ fileAsArray = (f.read()).split()
 
 del fileAsArray[0]
 
-startTimes = list(map(int, fileAsArray[::2]))
-endTimes = list(map(int, fileAsArray[1::2]))
-common = startTimes[0]
-startTimes[:] = [number - common for number in startTimes]
-endTimes[:] = [number - common for number in endTimes]
 
-i = 1
+allTimes = []
+q = 0
+while q < len(fileAsArray):
+    allTimes.append([int(fileAsArray[q]), int(fileAsArray[q+1])])
+    q += 2
+
+allTimes.sort()
+
+startTimes = []
+endTimes = []
+
+q = 0
+while q < len(allTimes):
+    startTimes.append(allTimes[q][0])
+    endTimes.append(allTimes[q][1])
+    q += 1
+
+
+common = startTimes[0]
+startTimes[:] = [number for number in startTimes]
+endTimes[:] = [number for number in endTimes]
+
+
+i = 0
 a = 0
-minStart = 0
+minStart = startTimes[0]
 maxEnd = endTimes[0]
 timeMilkingArray = []
+timeWithoutMilkingArray = []
 
 while i < len(startTimes):
     if startTimes[i] <= maxEnd:
-        # Replace end time
-        if endTimes[i] >= maxEnd:
+        if endTimes[i] > maxEnd:
             maxEnd = endTimes[i]
     else:
-        b = maxEnd-minStart
+        timeMilkingArray.append(maxEnd - minStart)
         minStart = startTimes[i]
+        timeWithoutMilkingArray.append(minStart - maxEnd)
         maxEnd = endTimes[i]
     i += 1
+else:
+    timeMilkingArray.append(maxEnd-minStart)
 
-print(b)
+maxWithMilking = 0 if len(timeMilkingArray) == 0 else max(timeMilkingArray)
+maxWithoutMilking = 0 if len(
+    timeWithoutMilkingArray) == 0 else max(timeWithoutMilkingArray)
+
+output = str(maxWithMilking) + " " + str(maxWithoutMilking) + '\n'
+w.write(output)
